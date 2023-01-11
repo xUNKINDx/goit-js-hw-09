8314b62e70a01df4ede1d714c0f28734594f9b47
Chumak Slavik <chumakslav@gmail.com>
2023-01-12 01:03:43 +0200

    hw 03 wip

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmitHandler);
+
+function onFormSubmitHandler(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  createPromises(
+    parseInt(delay.value),
+    parseInt(step.value),
+    parseInt(amount.value)
+  );
+}
+
+function createPromises(currentDelay, step, amount) {
+  for (let currentPosition = 1; currentPosition <= amount; currentPosition++) {
+    createPromise(currentPosition, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    currentDelay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
-    // Fulfill
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    });
   } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    });
   }
 }
