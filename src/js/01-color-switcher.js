99cbae0bb5c6a404d9c5485b4b2dc49f9444a3ba
Chumak Slavik <chumakslav@gmail.com>
2023-01-11 22:22:07 +0200

    rdy 1 hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+stopButton.disabled = true;
+
+startButton.addEventListener('click', () => {
+  stopButton.disabled = false;
+  startButton.disabled = true;
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  stopButton.disabled = true;
+  startButton.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
